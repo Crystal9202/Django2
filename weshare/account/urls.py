340710d58649819a880af421410d943b700112a8
Crystal <jpkoq18@gmail.com>
2022-07-28 23:14:15 +0800

    使用Django 內置的Login view

--- weshare/account/urls.py
+++ weshare/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     #post views
-    path('login',views.user_login , name="login")
+    path('login/' , auth_views.LoginView.as_view(template_name="account/login.html"),  name="login"),   #as_view()  是一個函數   #默認是使用registration底下的html，但也可以透過在as_view(templates_name="account/login.html") 指定執行那裡的html
 ]
