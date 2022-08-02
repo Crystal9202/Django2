ccf1abb94c317f160d9f0147696c0999c9945ed9
Crystal <jpkoq18@gmail.com>
2022-08-02 21:20:12 +0800

    做一個Login後的Dashboard頁面

--- weshare/account/urls.py
+++ weshare/account/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     #post views
     path('login/' , auth_views.LoginView.as_view(template_name="account/login.html"),  name="login"),   #as_view()  是一個函數   #默認是使用registration底下的html，但也可以透過在as_view(templates_name="account/login.html") 指定執行那裡的html
+    path('' , views.dashboard , name="dashboard"),
 ]
--- weshare/weshare/settings.py
+++ weshare/weshare/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+LOGIN_REDIRECT_URL = '/account'
+LOGIN_URL = '/account/login'
+LOGOUT_URL = '/account/logout'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
